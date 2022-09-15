e1657dd5f827147749e161992541873fcd8d9324
Renjith Varvila Reghu <vrrenjith5@gmail.com>
2022-09-15 22:49:27 +0530

    Update receiver_test.cpp

--- receiver_test.cpp
+++ receiver_test.cpp
@@ -8,6 +8,21 @@
 
 using namespace std;
 
+vector<int> temperature;
+vector<int> soc;
+
+vector<int> take_int(string str) {
+    stringstream ss(str);
+    vector<int> result;
+    char ch;
+    int tmp;
+    while(ss >> tmp) {
+        result.push_back(tmp);
+        ss >> ch;
+    }
+    return result;
+}
+
 int main()
 {
     int counter = 50;
@@ -16,8 +31,20 @@ int main()
     {
         string str;
         getline(cin, str);
+        vector<int> integers = take_int(str);
+        temperature.push_back(integers[0]);
+        soc.push_back(soc[1]);
         cout<<str<<endl;
     }
+    cout<"....................................................................."<<endl;
+    for(int i =0; i<50;i++) {
+        cout<< temperature[i]<<endl;
+    }
+   cout<"....................................................................."<<endl;
+        for(int i =0; i<50;i++) {
+        cout<< soc[i]<<endl;
+    }
+    
     cout<<"receiver end"<<endl;
     return 0;
 }
