21dc54cfdf615a577587c8f170f42f7e41e090ae
Renjith Varvila Reghu <vrrenjith5@gmail.com>
2022-09-15 14:53:58 +0530

    Update stream_data.py

--- stream_data.py
+++ stream_data.py
@@ -1,11 +1,11 @@
 from gen_temperature_stream import *
 from gen_soc_stream import *
 
-def stream_data(number_of_streams):
-    for i in range(number_of_streams):
-        temperature_list = temperature_stream()
-        print(temperature_list[i]\n)
+def stream_data():
+	temperature_list = temperature_stream()
+	soc_list = soc_stream()
+	for temperature, soc in zip(temperature_list, soc_list):
+		print("temperature:", temperature, " |  soc:", soc)
 
 if __name__ == '__main__':
-    number_of_streams = 1
-    stream_data(number_of_streams)
+    stream_data()
