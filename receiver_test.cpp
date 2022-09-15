c48efaec4ad8df6ef2b22e938adde6e882395f0f
Renjith Varvila Reghu <vrrenjith5@gmail.com>
2022-09-15 11:21:24 +0530

    Update receiver_test.cpp

--- receiver_test.cpp
+++ receiver_test.cpp
@@ -1,2 +1,23 @@
-#define CATCH_CONFIG_MAIN
-#include "test/catch.hpp"
+//#define CATCH_CONFIG_MAIN
+//#include "test/catch.hpp"
+
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int counter =5;
+    cout<<"receiver start"<<endl;
+    while(counter--)
+    {
+        string str;
+        getline(cin, str);
+        cout<<str<<endl;
+    }
+    cout<<"receiver end"<<endl;
+    return 0;
+}
