174d7c6b3e7ab6f88c084e6ff6bbd3c8f497b198
Renjith Varvila Reghu <vrrenjith5@gmail.com>
2022-09-19 16:16:23 +0530

    Update receiver.h

--- receiver/receiver.h
+++ receiver/receiver.h
@@ -2,6 +2,23 @@
 #ifndef __RECEIVER_H_
 #define __RECEIVER_H_
 
+#include <bits/stdc++.h>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+
 using namespace std;
+
+struct Sensor{
+  vector<int> temperatureList;
+  vector<int> stateOfChargeList;
+};
+
+Sensor receiveSensorDataFromConsole(int numberOfLinesToRead);
+vector<int> convertCsvStringToInteger(string stringToConvert);
+int findMaximumValue(vector<int> arryToFindMaxValue);
+int findMinimumValue(vector<int> arryToFindMinValue);
 
 #endif /*__RECEIVER_H_ */
