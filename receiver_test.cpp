3b4d79a1e355f460282c80b389b43bf8254ef797
Renjith Varvila Reghu <vrrenjith5@gmail.com>
2022-09-19 16:31:07 +0530

    Update receiver_test.cpp

--- receiver_test.cpp
+++ receiver_test.cpp
@@ -12,11 +12,22 @@
 using namespace std;
 
 Sensor sensorData;
+int maxTemperatureValue;
+int minTemperatureValue;
+int maxStateOfChargeValue;
+int minStateOfChargeValue;
 
 TEST_CASE("Test 1 : Read 0 lines from console, negative test case"){
   sensorData = receiveSensorDataFromConsole(0);
   REQUIRE(sensorData.temperatureList.size() == 0);
   REQUIRE(sensorData.stateOfChargeList.size() == 0);
-  
+  maxTemperatureValue = findMaximumValue(sensorData.temperatureList);
+  minTemperatureValue = findMinimumValue(sensorData.temperatureList);
+  REQUIRE(maxTemperatureValue == 0);
+  REQUIRE(minTemperatureValue == 0);  
+  maxStateOfChargeValue = findMaximumValue(sensorData.stateOfChargeList);
+  minStateOfChargeValue = findMinimumValue(sensorData.stateOfChargeList);
+  REQUIRE(maxStateOfChargeValue == 0);
+  REQUIRE(minStateOfChargeValue == 0);   
 }
 
