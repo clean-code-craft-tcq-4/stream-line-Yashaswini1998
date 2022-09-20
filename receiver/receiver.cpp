1c01468b81fd36b87bf9c1888dcc146cf6cdb298
Renjith Varvila Reghu <vrrenjith5@gmail.com>
2022-09-20 14:38:57 +0530

    Update receiver.cpp

--- receiver/receiver.cpp
+++ receiver/receiver.cpp
@@ -45,3 +45,16 @@ int findMinimumValue(vector<int> arryToFindMinValue) {
   }
   return minValue;
 }
+
+float simpleMovingAverage(vector<int> listToTest, int startIndex, int endIndex) {
+   int numberOfElements = (endIndex - startIndex)+1;
+   float average = 0.0;
+   float total = 0;
+   if((listToTest.size() >= 2) && (startIndex != endIndex)){
+     for(int counter = startIndex; counter<= endIndex; counter++) {
+       total += listToTest[counter];
+     }
+     average = total/numberOfElements;
+   }
+   return average;
+}
