60129a72e7337077cd30bcb666b847c5416c2057
Renjith Varvila Reghu <vrrenjith5@gmail.com>
2022-09-23 12:51:30 +0530

    Update compute_statistics.h

--- statistics/compute_statistics.h
+++ statistics/compute_statistics.h
@@ -12,6 +12,6 @@ public:
   int findMaximumValue(vector<int> arryToFindMaxValue);
   int findMinimumValue(vector<int> arryToFindMinValue);
   float simpleMovingAverage(vector<int> listToTest, int startIndex, int endIndex);
-}
+};
 
 #endif /*__COMPUTE_STATISTICS_H_ */
