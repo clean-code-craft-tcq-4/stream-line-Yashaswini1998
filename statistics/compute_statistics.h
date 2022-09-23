3159691bb7adc3f3f31a5144b09bc2e0b3596985
Renjith Varvila Reghu <vrrenjith5@gmail.com>
2022-09-23 12:11:08 +0530

    Create compute_statistics.h

---
+++ statistics/compute_statistics.h
@@ -0,0 +1,17 @@
+#ifndef __COMPUTE_STATISTICS_H_
+#define __COMPUTE_STATISTICS_H_
+
+#include <bits/stdc++.h>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int findMaximumValue(vector<int> arryToFindMaxValue);
+int findMinimumValue(vector<int> arryToFindMinValue);
+float simpleMovingAverage(vector<int> listToTest, int startIndex, int endIndex);
+
+#endif /*__COMPUTE_STATISTICS_H_ */
